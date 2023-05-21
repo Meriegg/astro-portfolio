b77a9841613bc4aeef54b52e999166704267615b
Meriegg <ya.boybeats1234@gmail.com>
2023-05-21 11:54:40 +0300

    Updated projects

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://mariodev.vercel.app/",
   integrations: [
     mdx({
       drafts: true,
